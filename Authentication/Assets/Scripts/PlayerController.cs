21cc9a9bd8586ae39c381ab94d2b0bb993276868
Ronno777 <smithryan954@gmail.com>
2025-11-13 22:35:03 -0700

    misc

--- Authentication/Assets/Scripts/PlayerController.cs
+++ Authentication/Assets/Scripts/PlayerController.cs
@@ -3,50 +3,78 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3f;
     private Rigidbody rig;
+
     private float startTime;
     private float timeTaken;
+
     private int collectablesPicked;
     public int maxCollectables = 10;
+
     private bool isPlaying;
 
+    public GameObject playButton;
+    public TextMeshProUGUI curTimeText;
 
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rig = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Do nothing if the run is not active
+        if (!isPlaying)
+            return;
+
+        // Movement
         float x = Input.GetAxis("Horizontal") * speed;
         float z = Input.GetAxis("Vertical") * speed;
         rig.linearVelocity = new Vector3(x, rig.linearVelocity.y, z);
+
+        // Timer text
+        curTimeText.text = (Time.time - startTime).ToString("F2");
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Collectable"))
+        // Ignore triggers while not playing
+        if (!isPlaying)
+            return;
+
+        if (other.CompareTag("Collectable"))
         {
             collectablesPicked++;
             Destroy(other.gameObject);
-            if (collectablesPicked == maxCollectables)
+
+            Debug.Log($"Picked {collectablesPicked}/{maxCollectables}");
+
+            if (collectablesPicked >= maxCollectables)
+            {
                 End();
+            }
         }
     }
+
     public void Begin()
     {
+        collectablesPicked = 0;
         startTime = Time.time;
         isPlaying = true;
+        playButton.SetActive(false);
     }
 
     void End()
     {
-        timeTaken = Time.time - startTime;
         isPlaying = false;
+        timeTaken = Time.time - startTime;
+
+        playButton.SetActive(true);
+
+        // Submit to leaderboard
+        Leaderboard.instance.SetLeaderboardEntry(
+            -Mathf.RoundToInt(timeTaken * 1000.0f)
+        );
     }
 }
