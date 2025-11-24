f2eca84b54fe5d31027f878f88feefa7d6d05884
Ronno7 <smithryan954@gmail.com>
2025-11-24 10:50:23 -0700

    Misc UI elements

--- Authentication/Assets/Scripts/TradeItem.cs
+++ Authentication/Assets/Scripts/TradeItem.cs
@@ -1,16 +1,38 @@
+using PlayFab;
+using PlayFab.ClientModels;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TradeItem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject tradeCanvas;
+    public TextMeshProUGUI inventoryText;
+    [HideInInspector]
+    public List<ItemInstance> inventory;
+    [HideInInspector]
+    public List<CatalogItem> catalog;
+
+    public static TradeItem instance;
+    void Awake() { instance = this; }
+
+    public UnityEvent onRefreshUI;
+
+    public void OnLoggedIn()
     {
-        
+        tradeCanvas.SetActive(true);
+        if (onRefreshUI != null)
+            onRefreshUI.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetInventory()
     {
-        
+
+    }
+
+    public void GetCatalog()
+    {
+
     }
 }
